0e335691cd415c809830e176a6acbc3fcca8e334
aida bennouna <aida.bennouna89@gmail.com>
2019-05-28 15:27:22 +0200

    olisting offerings

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -1,8 +1,10 @@
 class OfferingsController < ApplicationController
   def index
+    @offerings = Offering.all
   end
 
   def new
+    @offering = Offering.new(offering_attributes)
   end
 
   def edit
@@ -12,8 +14,21 @@ class OfferingsController < ApplicationController
   end
 
   def create
+    @offering = Offering.new(offering_params)
+    if @offering.save
+      redirect_to offering_path(@offering)
+    else
+      render "new"
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def offerings_params
+    params.require(:offering).permit(:first_name, :last_name, :email, :password)
+  end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# offering= Offering.new(offerings_attributes)
+
+
+
+Offering.create!(
+  name: "Ntm",
+  price_hour: 200,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh diam, scelerisque eu felis vel, aliquet congue lorem. Sed mollis congue ex. Vivamus diam nulla, viverra ut mauris ac, bibendum mollis augue. Aliquam in pellentesque nulla. Fusce consectetur diam ac mi vulputate sodales. Aenean gravida ultricies vulputate. In fermentum convallis interdum. Nullam vehicula porttitor quam id facilisis.",
+  genre: "Rap",
+  user_id: User.all.sample.id
+  )
+
+Offering.create!(
+  name: "Beattles",
+  price_hour: 300,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh diam, scelerisque eu felis vel, aliquet congue lorem. Sed mollis congue ex. Vivamus diam nulla, viverra ut mauris ac, bibendum mollis augue. Aliquam in pellentesque nulla. Fusce consectetur diam ac mi vulputate sodales. Aenean gravida ultricies vulputate. In fermentum convallis interdum. Nullam vehicula porttitor quam id facilisis.",
+  genre: "Rock",
+  user_id: User.all.sample.id
+)
+
+Offering.create!(
+  name: "Hall Jazz Band",
+  price_hour: 200,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh diam, scelerisque eu felis vel, aliquet congue lorem. Sed mollis congue ex. Vivamus diam nulla, viverra ut mauris ac, bibendum mollis augue. Aliquam in pellentesque nulla. Fusce consectetur diam ac mi vulputate sodales. Aenean gravida ultricies vulputate. In fermentum convallis interdum. Nullam vehicula porttitor quam id facilisis.",
+  genre: "Jazz ",
+  user_id: User.all.sample.id
+)
+
+Offering.create!(
+  name: "polo & pan",
+  price_hour: 400,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh diam, scelerisque eu felis vel, aliquet congue lorem. Sed mollis congue ex. Vivamus diam nulla, viverra ut mauris ac, bibendum mollis augue. Aliquam in pellentesque nulla. Fusce consectetur diam ac mi vulputate sodales. Aenean gravida ultricies vulputate. In fermentum convallis interdum. Nullam vehicula porttitor quam id facilisis.",
+  genre: "Electro",
+  user_id: User.all.sample.id
+)
+
+Offering.create!(
+  name: "Maluma",
+  price_hour: 1000,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nibh diam, scelerisque eu felis vel, aliquet congue lorem. Sed mollis congue ex. Vivamus diam nulla, viverra ut mauris ac, bibendum mollis augue. Aliquam in pellentesque nulla. Fusce consectetur diam ac mi vulputate sodales. Aenean gravida ultricies vulputate. In fermentum convallis interdum. Nullam vehicula porttitor quam id facilisis.
+",
+  genre: "Music of the World",
+  user_id: User.all.sample.id
+)
+
+
+
