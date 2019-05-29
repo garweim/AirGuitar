77f8f9535db4e5b426cdcca473ccb634bfa6a959
didierdewasseige <didierdewasseige@gmail.com>
2019-05-29 15:27:46 +0200

    solved

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -1,39 +1,49 @@
 class OfferingsController < ApplicationController
-  before_action :find_id, only: [ :show]
+  before_action :find_id, only: [:show]
   def index
     @offerings = Offering.all
   end
 
   def new
-
-
     @offering = Offering.new
   end
 
-  # def edit
-  # end
-
-  # def show
-  # end
-
   def create
-
     @offering = Offering.new(offerings_params)
-    if @offering.save
+    @offering.user = current_user
+    if @offering.save!
       redirect_to offering_path(@offering)
     else
       render :new
-
     end
   end
 
+  def show
+
+  end
+
+  # def edit
+  #   @offering = Offering.find(params[:id])
+  # end
+
+  # def update
+  #   if @offering.update(offering_params)
+  #     redirect_to edit_offering_path(@offering)
+  #   else
+  #     render :edit
+  #   end
+  # end
+
   def destroy
+    @offering = Offering.find(params[:id])
+    @offering.destroy
+    redirect_to root_path
   end
 
   private
 
   def offerings_params
-    params.require(:offering).permit(:name, :price_hour, :genre, :photo)
+    params.require(:offering).permit(:name, :price_hour, :genre, :picture, :description)
   end
 
   def find_id
