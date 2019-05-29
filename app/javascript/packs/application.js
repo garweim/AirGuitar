a464b29816825717f93e8cedc46bc6e21c6966ec
gary weimberg <gary1992@hotmail.com>
2019-05-29 11:46:26 +0200

    sign in and sign up done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,12 @@
 import "bootstrap";
-const displayAlertOnButtonClick = () => {
-  // TODO: Select the big green button
-  const button = document.querySelector("submit");
-  // TODO: Bind the `click` event to the button
-  button.addEventListener("click", (event) => {
-    alert("Offering created!");
-  });
-  // TODO: On click, display `Thank you!` in a JavaScript alert!
-};
+// const displayAlertOnButtonClick = () => {
+//   // TODO: Select the big green button
+//   const button = document.querySelector("submit");
+//   // TODO: Bind the `click` event to the button
+//   button.addEventListener("click", (event) => {
+//     alert("Offering created!");
+//   });
+//   // TODO: On click, display `Thank you!` in a JavaScript alert!
+// };
 
-displayAlertOnButtonClick();
+// displayAlertOnButtonClick();
