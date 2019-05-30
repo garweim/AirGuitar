0ed5e0648332b1580199353838708a1ebda41b33
didierdewasseige <didierdewasseige@gmail.com>
2019-05-30 16:01:56 +0200

    new booking and my bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,29 @@
 class BookingsController < ApplicationController
-  before_action :set_offering
-  # def index
-  # end
+  before_action :set_offering, only: [:new, :create]
+  def index
+    @bookings = Booking.all
+  end
 
   def new
+
     @booking = Booking.new
   end
 
   # def edit
   # end
 
-  # def show
-  # end
+  def show
+  end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.offering = @offering
-    @booking.save
-    redirect_to offering_path(@offering)
-    #POST
+    @booking.user = current_user
+    if @booking.save!
+      redirect_to offering_bookings_path(@offering)
+    else
+      render :new
+    end
   end
 
   # def destroy
@@ -31,6 +36,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:starting_date, :end_date)
+    params.require(:booking).permit(:starting_date, :end_date, :offering_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :offerings, only: [ :new, :edit, :show, :create, :destroy ]
   resources :offerings do
-    resources :bookings
+    resources :bookings, only: [ :new, :edit, :update, :create, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [ :destroy]
 end
